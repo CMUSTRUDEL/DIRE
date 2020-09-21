61618aeb41036aeb56cdd23eed2b944db3e5f45b
Edward J. Schwartz <eschwartz@cert.org>
2020-09-21 08:52:48 -0400

    Parallelize run_decompiler.py

--- dataset-gen/run_decompiler.py
+++ dataset-gen/run_decompiler.py
@@ -8,11 +8,13 @@
 import argparse
 import datetime
 import errno
+import multiprocessing
 import pickle
 import os
 import subprocess
 import sys
 import tempfile
+import tqdm
 
 statyre_dir = os.path.dirname(os.path.abspath(__file__))
 COLLECT = os.path.join(statyre_dir, 'decompiler_scripts', 'collect.py')
@@ -75,51 +77,50 @@ def run_decompiler(file_name, env, script, timeout=None):
         subprocess.call(['rm', '-f', f'{file_name}.i64'])
     return output
 
+def do_file(binary):
+    start = datetime.datetime.now()
+    #print(f"Started: {start}")
+    env['PREFIX'] = binary
+    file_path = os.path.join(args.binaries_dir, binary)
+    #print(f"Collecting from {file_path}")
+    with tempfile.NamedTemporaryFile() as collected_vars:
+        # First collect variables
+        env['COLLECTED_VARS'] = collected_vars.name
+        with tempfile.NamedTemporaryFile() as orig:
+            subprocess.check_output(['cp', file_path, orig.name])
+            # Timeout after 30 seconds for first run
+            try:
+                run_decompiler(orig.name, env, COLLECT, timeout=30)
+            except subprocess.TimeoutExpired:
+                print(f"{file_path} Timed out\n")
+                return
+            try:
+                if not pickle.load(collected_vars):
+                    print(f"No variables collected from {file_path}\n")
+                    return
+            except:
+                print(f"No variables collected from {file_path}\n")
+                return
+        # Make a new stripped copy and pass it the collected vars
+        with tempfile.NamedTemporaryFile() as stripped:
+            subprocess.call(['cp', file_path, stripped.name])
+            subprocess.call(['strip', '--strip-unneeded', stripped.name])
+            #print(f"{binary} stripped")
+            # Dump the trees.
+            # No timeout here, we know it'll run in a reasonable amount of
+            # time and don't want mismatched files
+            run_decompiler(stripped.name, env, DUMP_TREES)
+    #end = datetime.datetime.now()
+    #duration = end-start
+    #print(f"Duration: {duration}\n")
+
 # Create a temporary directory, since the decompiler makes a lot of additional
 # files that we can't clean up from here
 with tempfile.TemporaryDirectory() as tempdir:
     tempfile.tempdir = tempdir
 
-    # File counts for progress output
-    num_files = sum(1 for x in os.listdir(args.binaries_dir) \
-                    if os.path.isfile(os.path.join(args.binaries_dir, x)))
-    file_count = 1
+    tasks = os.listdir(args.binaries_dir)
 
-    for binary in os.listdir(args.binaries_dir):
-        print(f"File {file_count} of {num_files}")
-        file_count += 1
-        start = datetime.datetime.now()
-        print(f"Started: {start}")
-        env['PREFIX'] = binary
-        file_path = os.path.join(args.binaries_dir, binary)
-        print(f"Collecting from {file_path}")
-        with tempfile.NamedTemporaryFile() as collected_vars:
-            # First collect variables
-            env['COLLECTED_VARS'] = collected_vars.name
-            with tempfile.NamedTemporaryFile() as orig:
-                subprocess.check_output(['cp', file_path, orig.name])
-                # Timeout after 30 seconds for first run
-                try:
-                    run_decompiler(orig.name, env, COLLECT, timeout=30)
-                except subprocess.TimeoutExpired:
-                    print("Timed out\n")
-                    continue
-                try:
-                    if not pickle.load(collected_vars):
-                        print("No variables collected\n")
-                        continue
-                except:
-                    print("No variables collected\n")
-                    continue
-            # Make a new stripped copy and pass it the collected vars
-            with tempfile.NamedTemporaryFile() as stripped:
-                subprocess.call(['cp', file_path, stripped.name])
-                subprocess.call(['strip', '--strip-debug', stripped.name])
-                print(f"{binary} stripped")
-                # Dump the trees.
-                # No timeout here, we know it'll run in a reasonable amount of
-                # time and don't want mismatched files
-                run_decompiler(stripped.name, env, DUMP_TREES)
-        end = datetime.datetime.now()
-        duration = end-start
-        print(f"Duration: {duration}\n")
+    pool = multiprocessing.Pool()
+    for _ in tqdm.tqdm(pool.imap_unordered(do_file, tasks), total=len(tasks)):
+        pass
